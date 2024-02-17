8ee0860560df28ea181225ddd3043c46b198baa6
supreetvajjarmatti <supreetvajjaramatti@gmail.com>
2024-02-17 11:49:06 +0530

    style: Update Popular component layout and styles for improved image display

--- frontend/src/Components/Item/Item.jsx
+++ frontend/src/Components/Item/Item.jsx
@@ -4,14 +4,14 @@ import "./Item.css"
 function Item(props) {
   return (
     <div className='item'>
-        <img src={props.img} alt="" />
+        <img src={props.image} alt="" />
         <p>{props.name}</p>
         <div className='item-price'>
-            <div className='item-price-now'>
-                {props.new_price}
+            <div className='item-price-new'>
+                ${props.new_price}
             </div>
             <div className='item-price-old'>
-                {props.old_price}
+                ${props.old_price}
             </div>
         </div>
       
