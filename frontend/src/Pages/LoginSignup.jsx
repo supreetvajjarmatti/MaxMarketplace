f8459ce1b63c545ad63c8a39c9461445b6f3adac
supreetvajjarmatti <supreetvajjaramatti@gmail.com>
2024-02-19 22:17:08 +0530

    SignUp page is ready

--- frontend/src/Pages/LoginSignup.jsx
+++ frontend/src/Pages/LoginSignup.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './CSS/loginsignup.css'
 
 const LoginSignup = () => {
   return (
@@ -12,10 +13,11 @@ const LoginSignup = () => {
         </div>
         <button>Continue</button>
         <p className="loginsignup-login">
-          Already have an account? have n account?
-    
-          `
-        </p>
+          Already have an account?<span> Login </span></p>
+          <div className="loginsignup-agree">
+            <input type="checkbox" name="" id="" />
+            <p>By continuing, i agree to the terms of use & privacy policy.</p>
+          </div>
       </div>
       
     </div>
