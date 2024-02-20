bb80e1407521b54d64600d53faeae83455d0bfd8
supreetvajjarmatti <supreetvajjaramatti@gmail.com>
2024-02-20 22:09:23 +0530

    Changes in Product.jsx

--- frontend/src/Pages/Product.jsx
+++ frontend/src/Pages/Product.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ShopContext } from '../Context/ShopContext'
+import { useParams } from 'react-router-dom'
 
 const Product = () => {
+  const {all_product} = useContext(ShopContext)
+  const {productID} = useParams()
+  const product = all_product.fond((e)=>e.id===Number(productID))
   return (
     <div>
+      
       
     </div>
   )
