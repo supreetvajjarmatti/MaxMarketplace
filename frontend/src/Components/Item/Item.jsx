a6b92b2f8372ca5680f406ed2e49cbbab891d9cd
supreetvajjarmatti <supreetvajjaramatti@gmail.com>
2024-02-21 21:52:12 +0530

    Implement feature: Enable item click functionality with associated URLs

--- frontend/src/Components/Item/Item.jsx
+++ frontend/src/Components/Item/Item.jsx
@@ -4,7 +4,7 @@ import "./Item.css"
 function Item(props) {
   return (
     <div className='item'>
-        <img src={props.image} alt="" />
+        <Link to={`/product/${props.id}`}><img src={props.image} alt="" /></Link>
         <p>{props.name}</p>
         <div className='item-price'>
             <div className='item-price-new'>
--- frontend/src/Pages/Product.jsx
+++ frontend/src/Pages/Product.jsx
@@ -1,14 +1,15 @@
 import React, { useContext } from 'react'
 import { ShopContext } from '../Context/ShopContext'
 import { useParams } from 'react-router-dom'
+import Breadcrum from '../Components/Breadcrums/Breadcrum'
 
 const Product = () => {
   const {all_product} = useContext(ShopContext)
   const {productID} = useParams()
-  const product = all_product.fond((e)=>e.id===Number(productID))
+  const product = all_product.find((e)=>e.id===Number(productID))
   return (
     <div>
-      
+      <Breadcrum product={product}/>
       
     </div>
   )
